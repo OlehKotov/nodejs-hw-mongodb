2702fd6f0c136099158c5b3855e08d754cd08164
OlehKotov <mail.oleg456@gmail.com>
2024-05-22 22:54:36 +0300

    update

--- src/server.js
+++ src/server.js
@@ -24,18 +24,19 @@ export const startServer = () => {
 
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
-
     res.status(200).json({
       data: contacts,
+      message: "Successfully found contacts!",
     });
   });
+
 
   app.get('/contacts/:contactId', async (req, res) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
-
     res.status(200).json({
       data: contact,
+      message: `Successfully found contact with id ${contactId}!`,
     });
   });
 
@@ -45,15 +46,10 @@ export const startServer = () => {
     });
   });
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
+
 
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,9 +1,10 @@
-import { ContactsCollection } from '../db/models/contact.js';
+import { ContactsCollection } from '../db/contact.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
   return contacts;
 };
+
 
 export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
