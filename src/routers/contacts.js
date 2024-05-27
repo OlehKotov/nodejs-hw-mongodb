128fb3e7749e1f68602a7e99ffd362c64e715221
OlehKotov <mail.oleg456@gmail.com>
2024-05-27 10:31:53 +0300

    Update

---
+++ src/routers/contacts.js
@@ -0,0 +1,17 @@
+import { createContactController, deleteStudentController, getContactByIdController, getContactsController, patchContactController } from '../controllers/contacts.js';
+import { Router } from 'express';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = Router();
+
+router.get('/contacts', ctrlWrapper(getContactsController));
+
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteStudentController));
+
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -2,13 +2,19 @@ import { env } from './utils/env.js';
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
-  app.use(express.json());
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+    }),
+  );
   app.use(cors());
 
   app.use(
@@ -19,50 +25,11 @@ export const startServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      data: contacts,
-      message: 'Successfully found contacts!',
-    });
-  });
+  app.use(contactsRouter);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
+  app.use('*', notFoundHandler);
 
-    const expectedLength = 24;
-    if (contactId.length !== expectedLength) {
-      return res.status(400).json({
-        message: `Invalid contact ID format!`,
-      });
-    }
-
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      return res.status(404).json({
-        message: `Contact ${contactId} not found!`,
-      });
-    }
-
-    res.status(200).json({
-      data: contact,
-      message: `Successfully found contact with id ${contactId}!`,
-    });
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
