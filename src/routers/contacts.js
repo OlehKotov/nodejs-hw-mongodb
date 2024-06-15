a4b5e9a45c09644a3920aed6a54dd118fa91b6b2
OlehKotov <mail.oleg456@gmail.com>
2024-06-15 21:31:20 +0300

    Update

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -14,42 +14,38 @@ import {
 } from '../validation/contacts.js';
 import { validateMongoId } from '../middlewares/validateMongoId.js';
 import { authenticate } from '../middlewares/authenticate.js';
-import { checkUserId } from '../middlewares/checkUserId.js';
+
 
 const contactsRouter = Router();
 
 
 contactsRouter.use('/:contactId', validateMongoId('contactId'));
+
 contactsRouter.use(authenticate);
 
 contactsRouter.get(
   '/',
-  // checkUserId('userId'),
   ctrlWrapper(getContactsController),
 );
 
 contactsRouter.get(
   '/:contactId',
-  // checkUserId('userId'),
   ctrlWrapper(getContactByIdController),
 );
 
 contactsRouter.post(
   '/',
-  // checkUserId('userId'),
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
 
 contactsRouter.delete(
   '/:contactId',
-  // checkUserId('userId'),
   ctrlWrapper(deleteStudentController),
 );
 
 contactsRouter.patch(
   '/:contactId',
-  checkUserId('userId'),
   validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
